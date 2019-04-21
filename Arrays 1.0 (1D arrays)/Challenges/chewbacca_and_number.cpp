fbf43b270c64fe98389deceada0ce39ddba25193
mb11797 <mb.manasbhardwaj@gmail.com>
2019-04-22 02:39:45 +0530

    funnyFun

--- Arrays 1.0 (1D arrays)/Challenges/chewbacca_and_number.cpp
+++ Arrays 1.0 (1D arrays)/Challenges/chewbacca_and_number.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
 
+int main(){
+    char no[50];
+    cin>>no;
+
+    for(int i=0; i<strlen(no); i++){
+        int digit = no[i] - '0';
+        if(digit>=5){
+            if(i==0 && digit==9){
+                continue;
+            }
+            digit = 9 - digit;
+            no[i] = '0' + digit;
+        }
+    }
+
+    cout<<no;
+    return 0;
+}
