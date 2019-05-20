e0082edec6fd853c6bc16ece3393ac1775c14259
mb11797 <mb.manasbhardwaj@gmail.com>
2019-05-21 01:17:20 +0530

    funny trees

---
+++ Binary Tress/preorder_build_and_print.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node*left;
+    node*right;
+
+    node(int d){
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node*buildTree(){
+    int data;
+    cin>>data;
+
+    if(data==-1)
+        return NULL;
+
+    node*root = new node(data);
+    root->left = buildTree();
+    root->right = buildTree();
+
+    return root;
+}
+
+void printPre(node*root){
+    if(root==NULL)
+        return;
+
+    //Otherwise, print root first followed by subtrees(children)
+    cout<<root->data<<" ";
+    printPre(root->left);
+    printPre(root->right);
+}
+
+void printIn(node*root){
+    if(root==NULL)
+        return;
+
+    printIn(root->left);
+    cout<<root->data<<" ";
+    printIn(root->right);
+}
+
+void printPost(node*root){
+    if(root==NULL)
+        return;
+    printPost(root->left);
+    printPost(root->right);
+    cout<<root->data<<" ";
+}
+
+int main(){
+    node*root = buildTree();
+
+    printPre(root);
+    cout<<endl;
+    printIn(root);
+    cout<<endl;
+    printPost(root);
+    cout<<endl;
+
+    return 0;
+}
