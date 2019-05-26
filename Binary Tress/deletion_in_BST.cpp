198472bbd852374712484e0456b8e82b5b0e5c85
mb11797 <mb.manasbhardwaj@gmail.com>
2019-05-26 18:41:51 +0530

    bt deta rahunga ab toh

--- Binary Tress/deletion_in_BST.cpp
+++ Binary Tress/deletion_in_BST.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class node{
@@ -22,6 +23,34 @@ void inorder_print(node*root){
     inorder_print(root->right);
     return;
 }
+
+
+void bfs_awesome(node*root){
+    if(root==NULL)
+        return;
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node*f = q.front();
+        if(f==NULL){
+            cout<<endl;
+            q.pop();
+            if(!q.empty())
+                q.push(NULL);
+        }
+        else{
+            cout<<f->data<<" ";
+            q.pop();
+            if(f->left)
+                q.push(f->left);
+            if(f->right)
+                q.push(f->right);
+        }
+    }
+    return;
+}
+
 
 node*insert_in_BST(node*root, int data){
     if(root==NULL)
@@ -48,12 +77,67 @@ node*build(){
     return root;
 }
 
+node*delete_in_BST(node*root, int data){
+    if(root==NULL)
+        return NULL;
+    if(data < root->data){
+        root->left = delete_in_BST(root->left, data);
+        return root;
+    }
+    else if(data == root->data){
+        //3 cases
+        //case 1: 0 child
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+        //case 2: 1 child
+        else if(root->left != NULL && root->right == NULL){
+            node*temp = root->left;
+            delete root;
+            return temp;
+        }
+        else if(root->right != NULL && root->left == NULL){
+            node*temp = root->right;
+            delete root;
+            return temp;
+        }
+        //case 3: 2 child - find the inorder successor of the root node
+        else{
+            //search min in right subtree or max in left subtree to replace the root
+            //lets replace the min from right subtree, i.e, find the inorder successor of the root node and replace root->data with its data
+            node*replace = root->right;
+            while(replace->left != NULL){
+                replace = replace->left;
+            }
+            root->data = replace->data;
+            root->right = delete_in_BST(root->right, replace->data);
+            return root;
+        }
+    }
+    else{
+        root->right = delete_in_BST(root->right, data);
+        return root;
+    }
+
+
+}
 
 
 int main(){
     node*root = build();
     inorder_print(root);
     cout<<endl;
+    bfs_awesome(root);
+    cout<<endl;
 
+    int d;
+    cin>>d;
+
+    root = delete_in_BST(root, d);
+    inorder_print(root);
+    cout<<endl;
+    bfs_awesome(root);
+    cout<<endl;
     return 0;
 }
