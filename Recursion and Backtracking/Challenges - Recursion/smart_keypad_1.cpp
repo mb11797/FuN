35032e545cf138c9e101ddb555a9fa9ea158572a
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-04 07:17:19 +0530

    i will do it

---
+++ Recursion and Backtracking/Challenges - Recursion/smart_keypad_1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+//char keypad[][10] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wx", "yz"};
+string keypad[] = { " ", ".+@$", "abc", "def", "ghi", "jkl" , "mno", "pqrs" , "tuv", "wxyz" };
+
+void print_keypad_string_codes(char*in, char*out, int i, int j){
+    if(in[i]=='\0'){
+        out[j] = '\0';
+        cout<<out<<"\n";
+        return;
+    }
+
+    int digit = in[i] - '0';
+
+    if(digit==0)
+        print_keypad_string_codes(in, out, i+1, j);
+
+    for(int kp=0; keypad[digit][kp] != '\0'; kp++){
+        out[j] = keypad[digit][kp];
+        print_keypad_string_codes(in, out, i+1, j+1);
+    }
+    return;
+}
+
+int main(){
+    char in[100];
+    cin>>in;
+
+    char out[100];
+    print_keypad_string_codes(in, out, 0, 0);
+    cout<<endl;
+
+    return 0;
+}
+
