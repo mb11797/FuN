802eaca2a1a316b4fb284e3741db75f72ed54f6a
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-04 09:16:41 +0530

    i will do it

--- Arrays 1.0 (1D arrays)/Challenges/sum_of_2_arrays_element_by_element.cpp
+++ Arrays 1.0 (1D arrays)/Challenges/sum_of_2_arrays_element_by_element.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 
-void sum_2_arrays(int*a1, int n, int*a2, int m, int*sum){
-    int carry = 0;
-    int i, j, k;
+#define ll long long
+
+void sum_2_arrays(ll*a1, ll n, ll*a2, ll m, ll*sum){
+    ll carry = 0;
+    ll i, j, k;
     for(i=n-1, j=m-1, k=0; i>=0 || j>=0; i--, j--, k++){
-        int dig_sum;
+        ll dig_sum;
         if(i<0 && j>=0)
             dig_sum = a2[j] + carry;
         else if(j<0 && i>=0)
@@ -16,10 +18,12 @@ void sum_2_arrays(int*a1, int n, int*a2, int m, int*sum){
         sum[k] = dig_sum%10;
         carry = dig_sum/10;
     }
+    if(carry != 0)
+        sum[k++] = carry;
     sum[k] = -1;
 }
 
-void print_reverse_arr(int arr[]){
+void print_reverse_arr(ll arr[]){
     if(arr[0] == -1)
         return;
     print_reverse_arr(arr+1);
@@ -27,19 +31,19 @@ void print_reverse_arr(int arr[]){
 }
 
 int main(){
-    int n;
+    ll n;
     cin>>n;
-    int a1[n];
-    for(int i=0; i<n; i++)
+    ll a1[n];
+    for(ll i=0; i<n; i++)
         cin>>a1[i];
 
-    int m;
+    ll m;
     cin>>m;
-    int a2[m];
-    for(int i=0; i<m; i++)
+    ll a2[m];
+    for(ll i=0; i<m; i++)
         cin>>a2[i];
 
-    int sum[n+m];
+    ll sum[100000];
     sum_2_arrays(a1, n, a2, m, sum);
 
     print_reverse_arr(sum);
