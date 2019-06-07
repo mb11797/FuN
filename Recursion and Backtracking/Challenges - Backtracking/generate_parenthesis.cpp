120d041c308f07a93f27df3cc2bada82e5c436a5
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-08 00:03:52 +0530

    i will do it

--- Recursion and Backtracking/Challenges - Backtracking/generate_parenthesis.cpp
+++ Recursion and Backtracking/Challenges - Backtracking/generate_parenthesis.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
 using namespace std;
+
+void generate_parenthesis(int n, string osf, int open, int close){
+    if(close>=n){
+        cout<<osf<<endl;
+        return;
+    }
+    if(open<n)
+        generate_parenthesis(n, osf+"(", open+1, close);
+    if(close<open)
+        generate_parenthesis(n, osf+")", open, close+1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    generate_parenthesis(n, "", 0, 0);
+    return 0;
+}
