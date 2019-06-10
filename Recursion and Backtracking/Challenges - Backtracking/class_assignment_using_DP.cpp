357277e9a1e6e0a8404637c941081563daf89517
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-11 00:20:52 +0530

    i will do it

--- Recursion and Backtracking/Challenges - Backtracking/class_assignment_using_DP.cpp
+++ Recursion and Backtracking/Challenges - Backtracking/class_assignment_using_DP.cpp
@@ -3,27 +3,26 @@ using namespace std;
 
 int count_integers(int n){
     int a[n+1],b[n+1];
-    a[0]=0,a[1]=1;
-    b[0]=0,b[1]=1;
-    for(int i=2;i<=n;i++){
+    a[0] = b[0] = 1;
+    for(int i=1;i<n;i++){
         a[i]=a[i-1]+b[i-1];
         b[i]=a[i-1];
     }
-    return a[n]+b[n];
+    return a[n-1]+b[n-1];
 }
 
 int main(){
     int tc;
     cin>>tc;
 
-    while(tc--){
+	int i=0;
+    while(i<tc){
         int n;
         cin>>n;
 
-        int ans = count_integers(n);
-        cout<<endl;
-        cout<<"#"<<n<<" : "<<ans<<endl;
-    }
+        cout<<"#"<<i+1<<" : "<<count_integers(n)<<endl;
+		i++;
+	}
     return 0;
 }
 
