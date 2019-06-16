6aa3e8984e9b6e518b35b5af1a3213f363d432ea
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-17 00:34:58 +0530

    i will do it

--- Heaps/Challenges/merge_k_sorted_arrays.cpp
+++ Heaps/Challenges/merge_k_sorted_arrays.cpp
@@ -4,47 +4,51 @@
 #include<functional>
 using namespace std;
 
-typedef pair<int, pair<int, int>> pr;
+#define ll long long
+
+
+typedef pair<ll, pair<ll, ll>> pr;
 
 priority_queue<pr, vector<pr>, greater<pr>> pq;
 
-void merge_k_sorted_arrays(int arr[10][10], int out[1000], int k, int n){
+void merge_k_sorted_arrays(ll arr[10][10], ll out[1000], ll k, ll n){
 
-    for(int i=0; i<k; i++){
+    for(ll i=0; i<k; i++){
         pq.push({arr[i][0], {i, 0}});
     }
 
-    int z=0;
+    ll z=0;
     while(!pq.empty()){
         pr p = pq.top();
         pq.pop();
 
         out[z++] = p.first;
-        int i = p.second.first;
-        int j = p.second.second;
+        ll i = p.second.first;
+        ll j = p.second.second;
 
-        pq.push({arr[i][j+1], {i, j+1}});
+        if(j+1 < n)
+            pq.push({arr[i][j+1], {i, j+1}});
     }
     return;
 }
 
 
 int main(){
-    int k;
+    ll k;
     cin>>k;
 
-    int n;
+    ll n;
     cin>>n;
 
-    int arr[10][10];
-    int out[1000];
-    for(int i=0; i<k; i++)
-        for(int j=0; j<n; j++)
+    ll arr[10][10];
+    ll out[1000];
+    for(ll i=0; i<k; i++)
+        for(ll j=0; j<n; j++)
             cin>>arr[i][j];
 
     merge_k_sorted_arrays(arr, out, k, n);
 
-    for(int i=0; i<k*n; i++)
+    for(ll i=0; i<k*n; i++)
         cout<<out[i]<<" ";
 
     return 0;
