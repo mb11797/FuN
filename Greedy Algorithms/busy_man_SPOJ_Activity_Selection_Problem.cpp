ac6f4df997c9f15bd3b58fb015f0fbf070bcc47c
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-25 16:23:02 +0530

    i will do it

---
+++ Greedy Algorithms/busy_man_SPOJ_Activity_Selection_Problem.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void select_max_activities(int s[], int f[], int n){
+    int i=0;
+    cout<<i<<" ";
+
+    for(int j=1; j<n; j++){
+        if(s[j]>=f[i]){
+            cout<<j<<" ";
+            i=j;
+        }
+    }
+}
+
+int main(){
+    int s[] = {1,3,0,5,8,5};
+    int f[] = {2,4,6,7,9,9};
+
+    int n = sizeof(s)/sizeof(int);
+
+    select_max_activities(s, f, n);
+    return 0;
+}
+
+
+
+
+
+
