957fd1fa6e5e00ea2250a6d5ff71fd7b298a108a
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-25 13:04:45 +0530

    i will do it

--- Binary Tress/Challenges/generic_tree_find_sum_at_level_k.cpp
+++ Binary Tress/Challenges/generic_tree_find_sum_at_level_k.cpp
@@ -32,39 +32,48 @@ Node*build_tree(){
         }
     return temp;
 }
-
-void level_order_traversal_generic_tree(Node*root){
-    if(root==NULL)
-        return;
-
-    queue<Node*> q;
-    q.push(root);
-
-    while(!q.empty()){
-        int n = q.size();
-
-        while(n>0){
-            Node*f = q.front();
-            q.pop();
-            cout<<f->data<<" ";
-
-            for(int i=0; i<f->child.size(); i++){
-                q.push(f->child[i]);
-            }
-            n--;
-        }
-        cout<<endl;
-    }
-}
+//
+//void level_order_traversal_generic_tree(Node*root){
+//    if(root==NULL)
+//        return;
+//
+//    queue<Node*> q;
+//    q.push(root);
+//
+//    while(!q.empty()){
+//        int n = q.size();
+//
+//        while(n>0){
+//            Node*f = q.front();
+//            q.pop();
+//            cout<<f->data<<" ";
+//
+//            for(int i=0; i<f->child.size(); i++){
+//                q.push(f->child[i]);
+//            }
+//            n--;
+//        }
+//        cout<<endl;
+//    }
+//}
 
 int sum_at_level_k(Node*root, int k){
+    if(root==NULL)
+        return 0;
 
+    if(k==0)
+        return root->data;
 
+    int sum=0;
+    for(int i=0; i<root->child.size(); i++){
+        sum += sum_at_level_k(root->child[i], k-1);
+    }
+    return sum;
 }
 
 int main(){
     Node*root = build_tree();
-    level_order_traversal_generic_tree(root);
+//    level_order_traversal_generic_tree(root);
 
     int k;
     cin>>k;
