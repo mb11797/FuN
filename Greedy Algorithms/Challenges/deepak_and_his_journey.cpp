bfa48d1a7514c97b8d3ec28b55f681260f4de676
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-28 00:39:22 +0530

    i will do it

--- Greedy Algorithms/Challenges/deepak_and_his_journey.cpp
+++ Greedy Algorithms/Challenges/deepak_and_his_journey.cpp
@@ -1,33 +1,51 @@
 #include<iostream>
 using namespace std;
 
-int find_min_cost(int c[], int L[], int n){
+#define ll long long
 
-    int good_pp = 0;
+ll find_min_cost(ll c[], ll L[], ll n){
 
-    for(int i=0; i<n; i++){
+    ll good_pp = 0;
+    ll total_fuel = L[good_pp];
+    ll total_cost = 0;
 
-
+    for(ll i=1; i<n; i++){
+        if(c[good_pp]<c[i]){
+            total_fuel += L[i];
+        }
+        else{
+            total_cost += total_fuel * c[good_pp];
+            good_pp = i;
+            total_fuel = L[i];
+        }
+        if(i==n-1){
+            total_cost += total_fuel * c[good_pp];
+        }
     }
-
+    return total_cost;
 }
 
 
 int main(){
-    int n_chkpts;
-    cin>>n_chkpts;
+    ll tc;
+    cin>>tc;
 
-    int cost[n_chkpts];
-    for(int i=0; i<n_chkpts; i++)
-        cin>>cost[i];
+    while(tc--){
+        ll n_chkpts;
+        cin>>n_chkpts;
+
+        ll cost[n_chkpts];
+        for(ll i=0; i<n_chkpts; i++)
+            cin>>cost[i];
 
 
-    int litres[n_chkpts];
+        ll litres[n_chkpts];
 
-    for(int i=0; i<n_chkpts; i++)
-        cin>>litres[i];
+        for(ll i=0; i<n_chkpts; i++)
+            cin>>litres[i];
 
-    find_min_cost(cost, litres, n_chkpts);
+        cout<<find_min_cost(cost, litres, n_chkpts)<<endl;
+    }
 
     return 0;
 }
