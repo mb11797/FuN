8feec139fbc90bc1ad8863463dfac42db41d9c19
mb11797 <mb.manasbhardwaj@gmail.com>
2019-06-27 23:58:01 +0530

    i will do it

---
+++ Greedy Algorithms/Challenges/deepak_and_his_journey.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int find_min_cost(int c[], int L[], int n){
+
+    int good_pp = 0;
+
+    for(int i=0; i<n; i++){
+
+
+    }
+
+}
+
+
+int main(){
+    int n_chkpts;
+    cin>>n_chkpts;
+
+    int cost[n_chkpts];
+    for(int i=0; i<n_chkpts; i++)
+        cin>>cost[i];
+
+
+    int litres[n_chkpts];
+
+    for(int i=0; i<n_chkpts; i++)
+        cin>>litres[i];
+
+    find_min_cost(cost, litres, n_chkpts);
+
+    return 0;
+}
