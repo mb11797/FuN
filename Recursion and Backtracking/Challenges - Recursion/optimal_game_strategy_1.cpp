4a26db9ddece4310441700b9d1bdfeb6798cd8bd
mb11797 <mb.manasbhardwaj@gmail.com>
2019-07-02 23:58:10 +0530

    i will do it

---
+++ Recursion and Backtracking/Challenges - Recursion/optimal_game_strategy_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int find_the_winner(int coins[], int n){
+    int max;
+
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int coins[n];
+    for(int i=0; i<n; i++)
+        cin>>coins[i];
+
+    find_the_winner(coins, n);
+
+    return 0;
+}
