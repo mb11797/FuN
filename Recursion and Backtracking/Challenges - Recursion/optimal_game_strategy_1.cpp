09cc62bb06cd896e3834b5ca241cadec423cba32
mb11797 <mb.manasbhardwaj@gmail.com>
2019-07-03 23:46:02 +0530

    i will do it

--- Recursion and Backtracking/Challenges - Recursion/optimal_game_strategy_1.cpp
+++ Recursion and Backtracking/Challenges - Recursion/optimal_game_strategy_1.cpp
@@ -1,10 +1,44 @@
 #include<iostream>
 using namespace std;
 
-int find_the_winner(int coins[], int n){
-    int max;
+typedef pair<int, int> pairr;
 
+pairr find_the_winner(int coins[], int s, int e, char turn){
+    if(s>e){
+        return {0, 0};
+    }
 
+    pairr p = {0, 0};
+    pairr temp;
+    if(turn=='p'){
+        if(coins[s]>coins[e]){
+            p.first += coins[s];
+            s++;
+        }
+        else{
+            p.first += coins[e];
+            e--;
+        }
+
+        temp = find_the_winner(coins, s, e, 'n');
+    }
+    else if(turn=='n'){
+        if(coins[s]>coins[e]){
+            p.second += coins[s];
+            s++;
+        }
+        else{
+            p.second += coins[e];
+            e--;
+        }
+
+        temp = find_the_winner(coins, s, e, 'p');
+    }
+
+    p.first += temp.first;
+    p.second += temp.second;
+
+    return p;
 }
 
 int main(){
@@ -15,7 +49,8 @@ int main(){
     for(int i=0; i<n; i++)
         cin>>coins[i];
 
-    find_the_winner(coins, n);
+    pairr p = find_the_winner(coins, 0, n-1, 'p');
+    cout<<p.first<<endl;
 
     return 0;
 }
