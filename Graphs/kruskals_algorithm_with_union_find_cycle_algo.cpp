2cce080a0d9ef31072da830dac150b672e03399c
mb11797 <mb.manasbhardwaj@gmail.com>
2019-07-20 23:59:23 +0530

    i will do it

---
+++ Graphs/kruskals_algorithm_with_union_find_cycle_algo.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+class Edge{
+public:
+    int src;
+    int dest;
+    int weight;
+};
+
+bool cmp(Edge e1, Edge e2){
+
+}
+
+void kruskals(Edge*input, int n, int e){
+    sort(input, input+e, cmp);
+    Edge*output = new Edge[n-1];
+}
+
+int main(){
+    int n, e;
+    cin>>n>>e;
+
+    Edge*input = new Edge[e];
+    for(int i=0; i<e; i++){
+        int s, d, w;
+        cin>>s>>d>>w;
+        input[i].src = s;
+        input[i].dest = d;
+        input[i].weight = w;
+    }
+
+    return 0;
+}
