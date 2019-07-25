b1b84c55caf3c7e2d5b462cc68ac71dd9078daa1
mb11797 <mb.manasbhardwaj@gmail.com>
2019-07-25 23:59:42 +0530

    i will do it

--- Graphs/Bellman_Ford_SSSP.cpp
+++ Graphs/Bellman_Ford_SSSP.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+#define inf INT_MAX
+
 class Edge{
 public:
     int src, dest, weight;
@@ -28,8 +30,35 @@ public:
 
     void Bellman_Ford_SSSP(int src){
         int dist[V];
-        for(int i=0;)
+        for(int i=0; i<V; i++)
+            dist[i] = inf;
 
+
+        dist[src] = 0;
+
+        for(int i=0; i<=V-1; i++){
+            for(int j=0; j<E; j++){
+                int src = edge[j].src;
+                int dest = edge[i].dest;
+                int wt = edge[i].weight;
+
+                if(dist[src] != inf and (dist[src] + wt < dist[dest])){
+                    dist[dest] = dist[src] + wt;
+
+                }
+            }
+        }
+
+            for(int j=0; j<E; j++){
+                int src = edge[j].src;
+                int dest = edge[j].dest;
+                int wt = edge[j].weight;
+
+                if(dist[src] != inf and (dist[src] + wt < dist[dest])){
+                    cout<<"Negative Cycle Present"<<endl;
+                    return;
+                }
+            }
 
 
     }
@@ -37,7 +66,19 @@ public:
 };
 
 int main(){
+    int V, E;
+    cin>>V>>E;
+    Graph g(V, E);
 
+    for(int e=0; e<E; e++){
+        int s, d, w;
+        cin>>s>>d>>w;
+
+        g.add_edge(s, d, w, e);
+
+    }
+
+    g.Bellman_Ford_SSSP(0);
 
 
 
