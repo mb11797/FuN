48b17f1ec9ab10cfb92a54df9042d8f584bc5faa
mb11797 <mb.manasbhardwaj@gmail.com>
2019-07-29 23:59:30 +0530

    i will do it

---
+++ Dynamic Programming/reduce_no_to_one_in_min_step.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int reduce_no(int n){
+    if(n==1)
+        return 0;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int ans = reduce_no(n);
+    return 0;
+}
