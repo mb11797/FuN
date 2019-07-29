78580743d6726faf17f796f1df9b6f394af4552a
mb11797 <mb.manasbhardwaj@gmail.com>
2019-07-30 01:52:47 +0530

    i will do it

--- Dynamic Programming/reduce_no_to_one_in_min_step_Bottom_Up_DP.cpp
+++ Dynamic Programming/reduce_no_to_one_in_min_step_Bottom_Up_DP.cpp
@@ -24,6 +24,29 @@ int reduce_no_Memoization(int n){
     return memo[n];
 }
 
+int reduce_no_Bottom_Up_DP(int n){
+    int dp[10000];
+
+    //setting the base cases
+    dp[0] = 0;      dp[1] = 0;
+    dp[2] = 1;      dp[3] = 1;
+
+    //compute the ans for n
+    for(int cur_num=4; cur_num<=n; cur_num++){
+        int q1 = inf;
+        int q2 = inf;
+        int q3 = inf;
+
+        if(cur_num%3 == 0)      q1 = 1 + dp[cur_num/3];
+        if(cur_num%2 == 0)      q2 = 1 + dp[cur_num/2];
+        q3 = 1 + dp[cur_num-1];
+
+        dp[cur_num] = min(q1, min(q2, q3));
+    }
+
+    return dp[n];
+}
+
 
 int main(){
     int n;
@@ -32,5 +55,8 @@ int main(){
     fill(memo, memo + n + 1, -1);               //or use for loop to initialize or fill memo[] with -1
 
     cout<<"Min steps: "<<reduce_no_Memoization(n)<<endl;
+    cout<<"Min steps: "<<reduce_no_Bottom_Up_DP(n)<<endl;
+
     return 0;
 }
+
