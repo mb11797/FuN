b17f7292363e8f4e91137b16e4cea34923eda62a
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-04 23:50:15 +0530

    i will do it

--- Dynamic Programming/rod_cutting_to_maximize_profit.cpp
+++ Dynamic Programming/rod_cutting_to_maximize_profit.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int max_profit(int price_of_each_len[100], int total_len){
+    if(total_len==0)
+        return 0;
 
-
+    int best=0;
+    for(int len=1; len<=total_len; len++){
+        int net_profit = price_of_each_len[len] + max_profit(price_of_each_len, total_len-len);
+        best = max(best, net_profit);
+    }
+    return best;
 }
 
 int main(){
