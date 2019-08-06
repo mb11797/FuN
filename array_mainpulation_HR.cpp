a9fd37616af01a59c7f609312fd7e8b7c5b8cb51
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-06 23:58:08 +0530

    i will do it

---
+++ array_mainpulation_HR.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int array_manip(int n, int q[100][3]){
+
+
+}
+
+
+int main(){
+    int q[100][3] = {
+                    {1, 2, 100},
+                    {2, 5, 100},
+                    {3, 4, 100}
+    };
+
+    int n=5;
+
+    cout<<array_manip(n, q)<<endl;
+
+    return 0;
+}
+
