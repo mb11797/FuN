86c81b3252418f8914c1ac576c3360e781069ef1
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-07 18:26:23 +0530

    i will do it

---
+++ CodeChef/August Long Challenge/distribute_apples_efficient_correct.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int tc;
+    cin>>tc;
+
+    while(tc--){
+        int n, k;
+        cin>>n>>k;
+
+        //1st candidate will always have uniform distribution in each box
+        //we need to check only for second as follows:
+        if((n/k)%k==0)
+            cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
+
+    }
+    return 0;
+}
