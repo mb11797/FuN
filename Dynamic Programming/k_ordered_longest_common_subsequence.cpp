48708af423c652b40a4dd7a8f8733ced1d3c3413
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-15 23:59:29 +0530

    i will do it

---
+++ Dynamic Programming/k_ordered_longest_common_subsequence.cpp
@@ -0,0 +1,23 @@
+#icnlude<iostream>
+using namespace std;
+#define ll long long
+
+ll dp[2005][2005][8];
+ll n, m, k, a[2005], b[2005];
+
+
+ll k_ordered_lcs(ll i, ll j, ll k){
+
+
+}
+
+int main(){
+    memset(dp, -1, sizeof(dp));
+    ll k;
+    cin>>n>>m>>k;
+
+
+
+    return 0;
+}
+
