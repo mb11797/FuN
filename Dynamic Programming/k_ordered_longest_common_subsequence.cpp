2c04e825c9a4822f7bdd7843fef8fe16cea8544c
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-16 00:14:51 +0530

    i will do it

--- Dynamic Programming/k_ordered_longest_common_subsequence.cpp
+++ Dynamic Programming/k_ordered_longest_common_subsequence.cpp
@@ -1,4 +1,5 @@
-#icnlude<iostream>
+#include<iostream>
+#include<cstring>
 using namespace std;
 #define ll long long
 
@@ -7,8 +8,28 @@ ll n, m, k, a[2005], b[2005];
 
 
 ll k_ordered_lcs(ll i, ll j, ll k){
+    if(i==m || j==n)                        // if any string is finished, ans is zero
+        return 0;
 
+    //if current state has already been computed
+    if(dp[i][j][k] != -1){
+        return dp[i][j][k];
+    }
 
+    ll res=0;
+
+    if(a[i]==b[j]){
+        res = 1 + k_ordered_lcs(i+1, j+1, k);
+    }
+    else{
+        if(k>0){
+            res = 1 + k_ordered_lcs(i+1, j+1, k-1);
+        }
+
+        res = max(res, k_ordered_lcs(i, j+1, k));
+        res = max(res, k_ordered_lcs(i+1, j, k));
+    }
+    return dp[i][j][k] = res;
 }
 
 int main(){
@@ -16,7 +37,13 @@ int main(){
     ll k;
     cin>>n>>m>>k;
 
+    for(ll i=0; i<m; i++)
+        cin>>a[i];
 
+    for(ll j=0; j<n; j++)
+        cin>>b[j];
+
+    cout<<k_ordered_lcs(0, 0, k)<<endl;
 
     return 0;
 }
