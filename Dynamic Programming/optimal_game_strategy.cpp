b20e1be2fc60b0400e72a0ab3b2b5f239beaf2b1
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-20 00:14:16 +0530

    i will do it

--- Dynamic Programming/optimal_game_strategy.cpp
+++ Dynamic Programming/optimal_game_strategy.cpp
@@ -1,6 +1,7 @@
 //Memoization
 
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 #define ll long long
@@ -16,14 +17,22 @@ ll winner_score(ll i, ll j){
         return memo[i][j];
     }
 
+    int beg_choice = coins[i] + min(winner_score(i+2, j), winner_score(i+1, j-1));
+    int end_choice = coins[j] + min(winner_score(i, j-2), winner_score(i+1, j-1));
+
+    memo[i][j] = max(beg_choice, end_choice);
+
+    return memo[i][j];
 }
 
 int main(){
     //always even
     cin>>n;
 
+    memset(memo, -1, sizeof(memo));
+
     for(ll i=0; i<n; i++)
-        cin>>arr[i];
+        cin>>coins[i];
 
     cout<<winner_score(0, n-1)<<endl;
     return 0;
