7dc0a4b4e842da6e936c4e9ec1d99dc237d5e548
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-21 02:33:26 +0530

    i will do it

--- Dynamic Programming/Diwali_puzzle_3D_DP_with_bitmasks.cpp
+++ Dynamic Programming/Diwali_puzzle_3D_DP_with_bitmasks.cpp
@@ -6,20 +6,46 @@ using namespace std;
 #define mod 1000003
 
 ll n, k;
-ll dp[105][3][105];
+ll memo[105][3][105];
 
-ll f(ll i, ll prev, ll cnt){
+ll f(ll i, ll prev, ll count){
+    if(i == n){
+        if(count == k)
+            return 1;
 
+        return 0;
+    }
 
+    //Top-down DP - Memoization
+    if(memo[i][prev][count] != -1){
+        return memo[i][prev][count];
+    }
+
+    ll ans=0;
+    if(prev == 1){
+        ans = ans + f(i+1, 1, count+1);
+    }
+    else{
+        ans = ans + f(i+1, 1, count);
+    }
+    ans %= mod;
+
+    //when current element we put is 0
+    ans = ans + f(i+1, 0, count);
+    ans %= mod;
+
+    memo[i][prev][count] = ans;
+    return memo[i][prev][count];
 }
 
 int main(){
     int tc;
     cin>>tc;
-    memset(dp, -1, sizeof(dp));
+    memset(memo, -1, sizeof(memo));
     while(tc--){
+        memset(memo, -1, sizeof(memo));
         cin>>n>>k;
-//        cout<<f(1, )
+        cout<<(f(1, 0, 0) + f(1, 1, 0))%mod<<endl;                  //start by both choices, i.e, with first value either 0 or 1
     }
 
     return 0;
