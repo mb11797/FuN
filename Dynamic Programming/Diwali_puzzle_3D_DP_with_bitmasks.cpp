e30a5d897136968ad97584e8396373619ab923f7
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-20 23:59:37 +0530

    i will do it

---
+++ Dynamic Programming/Diwali_puzzle_3D_DP_with_bitmasks.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+#define ll long long
+#define mod 1000003
+
+ll n, k;
+ll dp[105][3][105];
+
+ll f(ll i, ll prev, ll cnt){
+
+
+}
+
+int main(){
+    int tc;
+    cin>>tc;
+    memset(dp, -1, sizeof(dp));
+    while(tc--){
+        cin>>n>>k;
+//        cout<<f(1, )
+    }
+
+    return 0;
+}
