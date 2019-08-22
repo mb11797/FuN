fc31362b1c89a5c2cb4bb1a1d6080414538689d6
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-23 01:45:45 +0530

    i will do it

--- Leetcode/zigzag_conversion.cpp
+++ Leetcode/zigzag_conversion.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 string convert(string s, int numRows){
+    if(numRows < 2)
+        return s;
     int numCols=0;
     int n_s_e_c = numRows-2;            //no of single element cols
     int n = s.size();
@@ -30,50 +32,54 @@ string convert(string s, int numRows){
     }
     cout<<numCols<<endl;
 
-    char mat[numRows][numCols];
-    for(int r=0; r<numRows; r++){
-        for(int c=0; c<numCols; c++){
+    char mat[1000][1000];
+    for(int r=0; r<numRows+2; r++){
+        for(int c=0; c<numCols+2; c++){
             mat[r][c] = '\0';
         }
     }
 
-
-    i=0;
+    int temp = numRows;
+    i=n;
     flag=1;
     int c=0;
     n_s_e_c = numRows - 2;
-    while(i < n){
+    while(i > 0){
         if(flag){
+            if(i < numRows){
+                numRows = i;
+            }
             for(int r=0; r<numRows; r++){
-                mat[r][c]=s[i];
-                i++;
+                mat[r][c]=s[n-i];
+                i--;
             }
             c++;
             flag=0;
         }
         else{
-            if(n-1-i < n_s_e_c){
-                n_s_e_c = n - 1 - i;
+            if(i < n_s_e_c){
+                n_s_e_c = i;
             }
             int dec = numRows-2;
             for(int j=0; j<n_s_e_c; j++){
-                mat[dec][c] = s[i];
+                mat[dec][c] = s[n-i];
                 dec--;
-                i++;
+                i--;
                 c++;
             }
             flag=1;
         }
     }
 
+    numRows = temp;
     string zigzag = "";
     int k=0;
     for(int r=0; r<numRows; r++){
         for(int c=0; c<numCols; c++){
-            if(mat[r][c] >= 'A' && mat[r][c] <= 'Z' || mat[r][c]=='\0'){
+//            if((mat[r][c] >= 'A' && mat[r][c] <= 'Z') || (mat[r][c] >= 'a' && mat[r][c] <= 'z') || mat[r][c]=='\0'){
                 if(mat[r][c] != '\0')
                     zigzag += mat[r][c];
-            }
+
             cout<<mat[r][c]<<" ";
         }
         cout<<endl;
@@ -85,7 +91,8 @@ string convert(string s, int numRows){
 int main(){
     int n;
     cin>>n;
-    cout<<convert("PAYPALISHIRING", n)<<endl;
+//    cout<<convert("PAYPALISHIRING", n)<<endl;
+    cout<<convert("Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers.", n)<<endl;
 //    cout<<"1\0 23"<<endl;
     return 0;
 }
