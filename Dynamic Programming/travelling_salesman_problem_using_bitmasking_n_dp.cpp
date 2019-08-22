bc4b88665c598514d80b9bbbb0defda26ba1eb8d
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-22 23:59:37 +0530

    i will do it

--- Dynamic Programming/travelling_salesman_problem_using_bitmasking_n_dp.cpp
+++ Dynamic Programming/travelling_salesman_problem_using_bitmasking_n_dp.cpp
@@ -4,6 +4,7 @@ using namespace std;
 //#define INT_MAX 999999
 
 int n=4;
+int dp[16][4];
 //Adjacency matrix which defines the graph
 int dist[10][10] = {
                     {0, 20, 42, 25},
@@ -21,6 +22,10 @@ int tsp(int mask, int pos){
         return dist[pos][0];
     }
 
+    //lookup
+    if(dp[mask][pos] != -1)
+        return dp[mask][pos];
+
     int ans = INT_MAX;
     //Try to go to an unvisited city
     for(int city=0; city<n; city++){
@@ -30,10 +35,15 @@ int tsp(int mask, int pos){
             ans = min(ans, new_ans);
         }
     }
-    return ans;
+    return dp[mask][pos] = ans;
 }
 
 int main(){
+    for(int i=0; i<(1<<n); i++){
+        for(int j=0; j<n; j++){
+            dp[i][j]=-1;
+        }
+    }
     //starting from city 1, i.e. mask=1 initially
     cout<<tsp(1, 0)<<endl;
 
