534e7f23b67abceaaf579081da82b6124e2a0d9c
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-30 12:04:03 +0530

    i will do it

---
+++ Dynamic Programming/egg_dropping_problem_using_top_down_dp_memoization.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cstring>
+using namespace  std;
+
+int memo[1000][1000];
+
+int min_trials_in_worst_case(int nf, int ne){
+    if(nf==0 || nf==1)
+        return nf;
+    if(ne == 1){
+        return nf;
+    }
+
+    if(memo[ne][nf] != -1)
+        return memo[ne][nf];
+
+    int ans = INT_MAX;
+    for(int i=1; i<=nf; i++){
+
+        //if egg breaks
+        int t1 = min_trials_in_worst_case(i-1, ne-1);
+
+        //if egg does not break
+        int t2 = min_trials_in_worst_case(nf-i, ne);
+
+        ans = min(ans, 1 + max(t1, t2));
+    }
+
+    return memo[ne][nf] = ans;
+}
+
+int main(){
+    memset(memo, -1, sizeof(memo));
+    int nf, ne;
+    cin>>nf>>ne;
+
+    cout<<min_trials_in_worst_case(nf, ne)<<endl;
+
+    return 0;
+}
