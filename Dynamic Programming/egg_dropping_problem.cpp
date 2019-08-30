491ec2ca9319d730f15af6f591728f1b7d2d6c71
mb11797 <mb.manasbhardwaj@gmail.com>
2019-08-30 11:48:17 +0530

    i will do it

--- Dynamic Programming/egg_dropping_problem.cpp
+++ Dynamic Programming/egg_dropping_problem.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
-using namespace std;
+using namespace  std;
 
 int min_trials_in_worst_case(int nf, int ne){
+    if(nf==0 || nf==1)
+        return nf;
     if(ne == 1){
         return nf;
     }
 
+    int ans = INT_MAX;
+    for(int i=1; i<=nf; i++){
 
+        //if egg breaks
+        int t1 = min_trials_in_worst_case(i-1, ne-1);
 
-    return 0;
+        //if egg does not break
+        int t2 = min_trials_in_worst_case(nf-i, ne);
+
+        ans = min(ans, 1 + max(t1, t2));
+    }
+
+    return ans;
 }
 
 int main(){
