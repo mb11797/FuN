cf1b4189a18c4d1f4e33e808b1f6ee6beebece4e
mb11797 <mb.manasbhardwaj@gmail.com>
2019-09-01 03:06:15 +0530

    i will do it

--- Dynamic Programming/cell_mitosis.cpp
+++ Dynamic Programming/cell_mitosis.cpp
@@ -1,7 +1,34 @@
 #include<iostream>
 using namespace std;
 
+long long solve_cell_mitosis(int n, int x, int y, int z){
+    long long *dp = new long long[n+1];
+
+    //Bottom Up DP
+    dp[0] = 0;
+    dp[1] = 0;
+
+    for(int i=2; i<=n; i++){
+
+        if(i%2 == 0){
+            dp[i] = min(dp[i/2] + x, dp[i-1] + y);
+        }
+        else{
+            dp[i] = min(dp[i-1] + y, dp[(i+1)/2] + x + z);
+        }
+    }
+
+    return dp[n];
+}
+
+
 int main(){
+    int n, x, y, z;
 
+    cin>>n;
+    cin>>x>>y>>z;
 
+    cout<<solve_cell_mitosis(n, x, y, z)<<endl;
+
+    return 0;
 }
