c17a9fd64c840ad6e693e16ded3b9cba5a905567
mb11797 <mb.manasbhardwaj@gmail.com>
2019-09-03 17:56:30 +0530

    i will do it

---
+++ Dynamic Programming/DP Webinar 1/fibonacci_LogN_efficient_top_down_using_hash_map.cpp
@@ -0,0 +1,30 @@
+//for 10^18 length nos
+
+#include<iostream>
+#include<map>
+using namespace std;
+#define ll long long
+map<int, int> F;
+
+ll f(ll n){
+    if(F.count(n)){
+        return F[n];
+    }
+
+    ll k = n/2;
+    if(n%2==0){
+        return F[n] = f(k)*f(k) + f(k-1)*f(k-1);
+    }
+    else{
+        return F[n] = f(k)*f(k+1) + f(k-1)*f(k);
+    }
+}
+
+int main(){
+    F[0] = F[1] = 1;
+    int n;
+    cin>>n;
+
+    cout<<f(n)<<endl;
+    return 0;
+}
