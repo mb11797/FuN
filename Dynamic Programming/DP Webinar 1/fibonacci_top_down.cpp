821eda2b31b850b3b40b7d88b9119d4659ed3fb1
mb11797 <mb.manasbhardwaj@gmail.com>
2019-09-03 16:25:37 +0530

    i will do it

--- Dynamic Programming/DP Webinar 1/fibonacci_top_down.cpp
+++ Dynamic Programming/DP Webinar 1/fibonacci_top_down.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
 
+int memo[1000];
+
+int fib(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+
+    if(memo[n] != -1)
+        return memo[n];
+
+    int ans = fib(n-1) + fib(n-2);
+
+    return memo[n] = ans;
+}
+
+int main(){
+    memset(memo, -1, sizeof(memo));
+    int n;
+    cin>>n;
+
+    cout<<fib(n)<<endl;
+    return 0;
+}
