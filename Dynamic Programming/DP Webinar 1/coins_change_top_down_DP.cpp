46aacd9762d4247b6ba50ae6962c4b5843364cb9
mb11797 <mb.manasbhardwaj@gmail.com>
2019-09-04 21:15:06 +0530

    i will do it

--- Dynamic Programming/DP Webinar 1/coins_change_top_down_DP.cpp
+++ Dynamic Programming/DP Webinar 1/coins_change_top_down_DP.cpp
@@ -3,20 +3,18 @@ using namespace std;
 
 #define ll long long
 
-int coins[100];
+ll coins[100];
 
-ll min_no_of_coins_in_change(int n, int n_coins){
+ll min_no_of_coins_in_change(ll n, ll n_coins){
+//    cout<<n<<"      ";
     //base case
     if(n <= 0){
         return 0;
     }
     ll ans = INT_MAX;
-    for(int i=0; i<n_coins; i++){
-        if(n > coins[i]){
+    for(ll i=0; i<n_coins; i++){
+        if(n >= coins[i]){
             ans = min(ans, 1 + min_no_of_coins_in_change(n-coins[i], n_coins));
-        }
-        else{
-            continue;
         }
     }
     return ans;
@@ -27,7 +25,7 @@ int main(){
     coins[1] = 7;
     coins[2] = 10;
 
-    int n;
+    ll n;
     cin>>n;
 
     cout<<min_no_of_coins_in_change(n, 3)<<endl;
