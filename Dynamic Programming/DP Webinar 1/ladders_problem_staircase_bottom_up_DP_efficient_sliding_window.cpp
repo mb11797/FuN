a1498b1125a4fb6df8ee9898dd47e1327c5c41ed
mb11797 <mb.manasbhardwaj@gmail.com>
2019-09-04 15:02:18 +0530

    i will do it

--- Dynamic Programming/DP Webinar 1/ladders_problem_staircase_bottom_up_DP_efficient_sliding_window.cpp
+++ Dynamic Programming/DP Webinar 1/ladders_problem_staircase_bottom_up_DP_efficient_sliding_window.cpp
@@ -7,10 +7,14 @@ int f(int n, int k){
     dp[0] = 1;
     dp[1] = 1;
 
-    for(int i=2; i<=n; i++){
-        dp[i] = 2*dp[i-1] - dp[i-(k+1)];
+    for(int i=2; i<=k; i++){
+        dp[i] = 2*dp[i-1];                          //for 2<=i<=k => dp[i] = 2*dp[i-1]
     }
 
+    for(int i=k+1; i<=n; i++){
+        dp[i] = 2*dp[i-1] - dp[i-(k+1)];            //for k<i<=n => dp[i] = dp[i-1] - dp[i-(k+1)} + dp[i-1]
+    }
+    return dp[n];
 }
 
 int main(){
