3ab9c281ac82d11e3a3a5d4493571e95932e3dd0
mb11797 <mb.manasbhardwaj@gmail.com>
2019-09-06 02:03:06 +0530

    i will do it

--- Dynamic Programming/DP Webinar 1/wine_n_max_profit_bottom_up_DP.cpp
+++ Dynamic Programming/DP Webinar 1/wine_n_max_profit_bottom_up_DP.cpp
@@ -6,17 +6,22 @@ ll wines[100];
 ll dp[101][101] = {0};
 
 ll max_profit(ll n){
-
+    ll yr = n;
     //for be<en  => dp[be][en] = 0
     //for be==en = i => dp[i][i] represents the cost of selling ith wine in the end => dp[i][i] = wines[i]*n
     for(ll i=0; i<n; i++){
-        dp[i][i] = n*wines[i];
+        dp[i][i] = wines[i]*yr;
     }
 
-    ll yr=n-1;
-    for(ll be=0; be<n-1; be++){
-        for(ll en=be+1; en<n; en++){
-            dp[be][en] = max(wines[be]*yr + dp[be+1][en], wines[en]*yr + dp[be][en-1]);
+    yr--;
+
+    for(ll len=2; len<=n; len++){
+        ll start = 0;
+        ll end = n - len;
+        while(start<=end){
+            ll end_window = start + len - 1;
+            dp[start][end_window] = max(wines[start]*yr + dp[start+1][end_window], wines[end_window]*yr + dp[start][end_window-1]);
+            start++;
         }
         yr--;
     }
