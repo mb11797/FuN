b6b9716abf17aa9850883b9818fb0c6c8d92bfc2
mb11797 <mb.manasbhardwaj@gmail.com>
2019-09-08 23:09:49 +0530

    i will do it

---
+++ largest_area_of_rectangle_contating_only_1s_using_DP.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int arr[100][100];
+
+int max_area_rectangle(int rows, int cols){
+    stack<pair<int, int>> s;
+
+    int max_area=0;
+    for(int r=0; r<rows; r++){
+        s.push(arr[r][0], 0);
+        for(int c=0; c<cols; c++){
+            if(arr[r][c] > s.top().first){
+                s.push(arr[r][c], c);
+            }
+            else{
+                max_area = max(max_area, )
+            }
+        }
+    }
+}
+
+int main(){
+    int rows, cols;
+    cin>>rows>>cols;
+
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<cols; j++)
+            cin>>arr[i][j];
+
+    cout<<max_area_rectangle(rows, cols)<<endl;
+
+    return 0;
+}
